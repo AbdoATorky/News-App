ca37be84856ac06478ab4e82cd98b9fbd159b692
AbdoATorky <abdalsalamtorky@gmail.com>
2026-04-16 20:55:35 +0200

    Create article list item layout and implement Retrofit network call in MainActivity

--- app/src/main/java/com/abs/newsapp/MainActivity.kt
+++ app/src/main/java/com/abs/newsapp/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.abs.newsapp.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewbinding: ActivityMainBinding
 
-    // https://newsapi.org
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,5 +26,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    private fun loadNews(){
+        val retrofit = Retrofit
+            .Builder()
+            .baseUrl("https://newsapi.org")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val c = retrofit.create(NewsCallable::class.java)
+        c.getNews().enqueue(object :Callback<News>{
+            override fun onResponse(call: Call<News?>, response: Response<News?>
+            ) {
+                val news = response.body()
+                val articles = news?.articles
+            }
+
+            override fun onFailure(call: Call<News?>, throwable: Throwable
+            ) {
+
+            }
+        })
+
     }
 }
